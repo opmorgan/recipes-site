de91840e7985e4a669b02698e88f40ffdafda26a
Owen Morgan <owen.morgan95@gmail.com>
2022-09-26 00:08:10 -0400

    Add date updated to homepage

--- base/views.py
+++ base/views.py
@@ -12,12 +12,13 @@ class HomeView(generic.ListView):
 
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        # context["latest_recipes_list"] = Recipe.objects.filter(
-        #             created_at__lte=timezone.now()).order_by('-created_at')[:10]
+
         context["latest_recipe_list"] = Recipe.objects.filter(
-                    created_at__lte=timezone.now()).order_by('-created_at')[:10]
+                created_at__lte=timezone.now()).order_by('-created_at')[:10]
         context["latest_tag_list"] = Tag.objects.filter(
-                    created_at__lte=timezone.now()).order_by('-created_at')[:10]
+                created_at__lte=timezone.now()).order_by('-created_at')[:10]
+        context["latest_recipe"] = Recipe.objects.filter(
+                created_at__lte=timezone.now()).order_by('-created_at')[:1]
 
         return context
 
