6fcce536c0a9a67f79eebd42732e8c0b6f1d6e2c
Owen Morgan <owen.morgan95@gmail.com>
2022-09-27 23:47:11 -0400

    Fix logic on homepage for when to show 'see all recipes'

--- base/views.py
+++ base/views.py
@@ -15,6 +15,8 @@ class HomeView(generic.ListView):
 
         n_recent_recipes = 10
         n_recent_tags = 10
+        n_all_recipes = Recipe.objects.all().count()
+        n_all_tags = Tag.objects.all().count()
         latest_recipes_list = Recipe.objects.filter(
                 created_at__lte=timezone.now()).order_by('-created_at')[:n_recent_recipes]
         latest_tags_list = Tag.objects.filter(
@@ -22,11 +24,14 @@ class HomeView(generic.ListView):
         latest_recipe =  Recipe.objects.filter(
                 created_at__lte=timezone.now()).order_by('-created_at')[:1]
 
+
         context["latest_recipes_list"] = latest_recipes_list
         context["latest_tags_list"] = latest_tags_list
         context["latest_recipe"] = latest_recipe
         context["n_recent_recipes"] = n_recent_recipes
         context["n_recent_tags"] = n_recent_tags
+        context["n_all_recipes"] = n_all_recipes
+        context["n_all_tags"] = n_all_tags
 
         return context
 
