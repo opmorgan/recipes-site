36d0c65ff94ee4fb15f74abd07eb4c23d1cf6c42
Owen Morgan <owen.morgan95@gmail.com>
2022-09-27 22:13:19 -0400

    Add 'new recipes' header and 'see all' button to homepage (recipe index sill shows only recent recipes)

--- base/views.py
+++ base/views.py
@@ -13,12 +13,20 @@ class HomeView(generic.ListView):
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
 
-        context["latest_recipe_list"] = Recipe.objects.filter(
-                created_at__lte=timezone.now()).order_by('-created_at')[:10]
-        context["latest_tag_list"] = Tag.objects.filter(
-                created_at__lte=timezone.now()).order_by('-created_at')[:10]
-        context["latest_recipe"] = Recipe.objects.filter(
+        n_recent_recipes = 10
+        n_recent_tags = 10
+        latest_recipe_list = Recipe.objects.filter(
+                created_at__lte=timezone.now()).order_by('-created_at')[:n_recent_recipes]
+        latest_tag_list = Tag.objects.filter(
+                created_at__lte=timezone.now()).order_by('-created_at')[:n_recent_tags]
+        latest_recipe =  Recipe.objects.filter(
                 created_at__lte=timezone.now()).order_by('-created_at')[:1]
+
+        context["latest_recipe_list"] = latest_recipe_list
+        context["latest_tag_list"] = latest_tag_list
+        context["latest_recipe"] = latest_recipe
+        context["n_recent_recipes"] = n_recent_recipes
+        context["n_recent_tags"] = n_recent_tags
 
         return context
 
