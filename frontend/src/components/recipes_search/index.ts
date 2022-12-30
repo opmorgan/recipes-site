f050451419368c2a3983dc33ff5a17f2737da265
Owen Morgan <owen.morgan95@gmail.com>
2022-12-30 18:17:27 -0500

    Sketch Elm architecture for search lit component

--- frontend/src/components/recipes_search/index.ts
+++ frontend/src/components/recipes_search/index.ts
@@ -1,5 +1,5 @@
-import {LitElement, html, css} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import {LitElement, html, css, nothing, TemplateResult} from 'lit';
+import {customElement, property, state} from 'lit/decorators.js';
 import {repeat} from 'lit/directives/repeat.js';
 import {styles} from './styles.css'; // (Q) Why not "./syles.css.ts"?
 import Fuse from 'fuse.js'; // Fzf search library
@@ -23,23 +23,96 @@ type RecipePart = {
   fields: Pick<RecipeQueryObject['fields'], 'title' | 'description'>;
 };
 
+enum RecipeSearchResultsState {
+  NO_QUERY,
+  HAS_RESULTS,
+  NO_RESULTS,
+}
+
+enum Action {
+  UPDATE_QUERY,
+  UPDATE_RESULTS_STATE,
+}
+
+type MsgVariant<V> = {
+  action: Action;
+  value: V;
+};
+
+type Msg =
+  | MsgVariant<string>
+  | MsgVariant<RecipeSearchResultsState>;
+
+const UpdateQuery = (value: string): MsgVariant<string> => ({
+  action: Action.UPDATE_QUERY,
+  value,
+});
+
+const UpdateResultsState =
+  (value: RecipeSearchResultsState): MsgVariant<RecipeSearchResultsState> => ({
+    action: Action.UPDATE_RESULTS_STATE,
+    value,
+  });
+
+
 @customElement(RECIPES_SEARCH_TAG)
 export class RecipesSearch extends LitElement {
-  // (Q) what does "static styles = css`...`;" do?
+  // (Q) what does "static styles = css`...`;" (from lit tutorial) do?
   @property({type: Array})
-  recipes: RecipePart[] = [];
+  readonly recipes: RecipePart[] = [];
 
   @property()
   // Query is changed on user input, as set in the render function
   query = '';
 
+  @state()
+  private resultsState: RecipeSearchResultsState = RecipeSearchResultsState.NO_QUERY;
+
+
   static get styles() {
     return styles;
   }
 
+  // connectedCallback() {
+  //   window.addEventListner('click', (event: Event) => {
+  //     if (this.resultsState === RecipeSearchResultsState.NO_QUERY) return;
+  //     // Check to see if a user clicked the box or not.
+  //     // if (no) this.handleMsg(UpdateResultsState.HIDDEN);
+  //   });
+  // }
+
+  private handleMsg(msg: Msg) {
+    switch (msg.action) {
+      case Action.UPDATE_QUERY: {
+        this.query = msg.value;
+
+        if (!this.query) {
+          this.handleMsg(UpdateResultsState(RecipeSearchResultsState.NO_QUERY));
+          break;
+        }
+
+        if (!this.searchResults().length) {
+          this.handleMsg(UpdateResultsState(RecipeSearchResultsState.NO_RESULTS));
+          break;
+        }
+
+        this.handleMsg(UpdateResultsState(RecipeSearchResultsState.HAS_RESULTS));
+        break;
+      }
+
+      case Action.UPDATE_RESULTS_STATE: {
+        this.resultsState = msg.value;
+        break;
+      }
+
+      default:
+        const exhaustive: never = msg.action;
+        throw Error(`Unhandled case: ${exhaustive}`);
+    }
+  }
+
   searchResults(): RecipePart[] {
     if (!this.query.length) return [];
-    console.log(this.recipes, typeof this.recipes);
 
     const fuseOptions = {
       minMatchCharLength: 1,
@@ -68,6 +141,31 @@ export class RecipesSearch extends LitElement {
     `;
   }
 
+  private renderSearchResults() {
+   const resultsWrapper = (markup: TemplateResult) => html`
+      <div class="recipes-search__results">
+        ${markup}
+      </div>
+    `;
+
+    switch (this.resultsState) {
+      case RecipeSearchResultsState.NO_RESULTS:
+        return resultsWrapper(html`
+            <strong>There are no recipes matching '${this.query}'.</strong>
+        `);
+      case RecipeSearchResultsState.NO_QUERY:
+        return nothing;
+      case RecipeSearchResultsState.HAS_RESULTS:
+        return resultsWrapper(html`
+          ${repeat(
+            this.searchResults(),
+            element => element.fields.title,
+            this.renderRecipe
+          )}
+        `);
+    }
+  }
+
   render() {
     return html`
       <div class="recipes-search">
@@ -77,16 +175,11 @@ export class RecipesSearch extends LitElement {
           placeholder="Search all recipes"
           .value=${this.query}
           @input=${(e: Event) => {
-            this.query = (e.target as HTMLInputElement).value || '';
+            const query = (e.target as HTMLInputElement).value || '';
+            this.handleMsg(UpdateQuery(query));
           }}
         />
-        <div class="recipes-search__results">
-          ${repeat(
-            this.searchResults(),
-            element => element.fields.title,
-            this.renderRecipe
-          )}
-        </div>
+        ${this.renderSearchResults()}
       </div>
     `;
   }
