380a6a5931c8a5a5f2ccae6fb04fa56325e8e91a
Owen Morgan <owen.morgan95@gmail.com>
2023-03-04 02:08:48 +0000

    Add normalize css; update settings to use media, static folders as env vars

--- base/settings.py
+++ base/settings.py
@@ -144,10 +144,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+# STATIC_ROOT = '/var/www/pub/honeybit.cooking/static/'
+STATIC_ROOT = env('STATIC_ROOT')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),] # To use base level "static" directory
 MEDIA_URL = '/assets/'
-MEDIA_ROOT = BASE_DIR/'assets'
+# MEDIA_ROOT = BASE_DIR/'assets'
+MEDIA_ROOT = env('MEDIA_ROOT')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
