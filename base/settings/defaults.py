81090d3ef47761124757e0b6d18b119430ec336d
Owen Morgan <owen.morgan95@gmail.com>
2023-03-13 02:21:19 +0000

    Hotfix: remove staticfiles from installed apps in defaults config

--- base/settings/defaults.py
+++ base/settings/defaults.py
@@ -56,7 +56,7 @@ INSTALLED_APPS = [
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
-        'django.contrib.staticfiles',
+        # 'django.contrib.staticfiles',
         # 'recipes.apps.RecipesConfig',
         'base',
         'recipes',
