363640a86fb16655a75d82cfa6a9c2287edccfbd
Owen Morgan <owen.morgan95@gmail.com>
2023-03-14 00:51:40 -0400

    Move staticfiles to INSTALLED_APPS in default config (it's needed in dev as well as production)

--- base/settings/defaults.py
+++ base/settings/defaults.py
@@ -57,7 +57,7 @@ INSTALLED_APPS = [
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
-        # 'django.contrib.staticfiles',
+        'django.contrib.staticfiles',
         # 'recipes.apps.RecipesConfig',
         'base',
         'recipes',
